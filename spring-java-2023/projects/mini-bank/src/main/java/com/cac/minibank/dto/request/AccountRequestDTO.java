aab9b20a7bad3e41317d1612c9541d6730701b9e
NachoJ12 <nacho.justel12@gmail.com>
2023-07-07 11:33:49 -0300

    Update AccountRequestDTO

--- spring-java-2023/projects/mini-bank/src/main/java/com/cac/minibank/dto/request/AccountRequestDTO.java
+++ spring-java-2023/projects/mini-bank/src/main/java/com/cac/minibank/dto/request/AccountRequestDTO.java
@@ -4,11 +4,14 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 public class AccountRequestDTO {
     @NotNull
     private Long customerId;
     private int currencyId;
+    List<Long> jointAccountHoldersId;
 
 }
