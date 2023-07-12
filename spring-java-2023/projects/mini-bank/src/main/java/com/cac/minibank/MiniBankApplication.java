2d0b45d0b7a45b35d4aeb0ed1b17f13d3d9c68f2
NachoJ12 <nacho.justel12@gmail.com>
2023-07-12 12:10:34 -0300

    Add in-app currency data initialization

--- spring-java-2023/projects/mini-bank/src/main/java/com/cac/minibank/MiniBankApplication.java
+++ spring-java-2023/projects/mini-bank/src/main/java/com/cac/minibank/MiniBankApplication.java
@@ -1,8 +1,12 @@
 package com.cac.minibank;
 
+import com.cac.minibank.model.Currency;
+import com.cac.minibank.repository.CurrencyRepository;
+import org.springframework.boot.CommandLineRunner;
 import  org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -13,5 +17,16 @@ public class MiniBankApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MiniBankApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(
+		CurrencyRepository currencyRepository
+	){
+		return args -> {
+			currencyRepository.save(new Currency(1, "Euro", "EUR"));
+			currencyRepository.save(new Currency(2, "Dolar", "USD"));
+		};
+	}
+
 
 }
