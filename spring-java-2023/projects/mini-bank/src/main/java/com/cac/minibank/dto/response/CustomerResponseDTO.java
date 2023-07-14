a4d220252064a34bfc6a50e506a4335ddb0dfe2a
NachoJ12 <nacho.justel12@gmail.com>
2023-07-14 19:50:10 -0300

    Update CustomerResponseDTO to use AddressDTO instead of Address

--- spring-java-2023/projects/mini-bank/src/main/java/com/cac/minibank/dto/response/CustomerResponseDTO.java
+++ spring-java-2023/projects/mini-bank/src/main/java/com/cac/minibank/dto/response/CustomerResponseDTO.java
@@ -1,6 +1,6 @@
 package com.cac.minibank.dto.response;
 
-import com.cac.minibank.model.Address;
+import com.cac.minibank.dto.AddressDTO;
 import lombok.*;
 
 import java.util.List;
@@ -14,7 +14,7 @@ public class CustomerResponseDTO {
     private String surname;
     private String phone;
     private String email;
-    private Address address;
+    private AddressDTO addressDTO;
     private List<Long> accountsId;
     private List<Long> jointAccountsId;
 
